811500dc1199bc31651077da4f3f6abe092d1772
Mark Murray <mark@murray.xyz>
2019-06-01 14:06:18 +0100

    feat(headline): create headline component

---
+++ src/components/headline/index.tsx
@@ -0,0 +1,18 @@
+import React, { FC } from 'react'
+import styled from 'styled-components'
+
+type HeadlineProps = {}
+
+const StyledHeadline = styled.h1`
+  font-size: 100px;
+  font-weight: 900;
+  letter-spacing: 0.05em;
+  line-height: 1.4;
+  margin: 0;
+`
+
+const Headline: FC<HeadlineProps> = ({ children }) => (
+  <StyledHeadline>{children}</StyledHeadline>
+)
+
+export default Headline
