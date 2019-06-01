53cae44ce3fe63ce4daddce042cbfada2e3289e1
Mark Murray <mark@murray.xyz>
2019-06-01 12:08:19 +0100

    chore(storybook): configure Storybook name, url, and theme

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,18 @@
-import { configure } from '@storybook/react'
+import { addParameters, configure } from '@storybook/react'
+import { themes } from '@storybook/theming'
 
 const req = require.context('../src/components', true, /\/stories.tsx$/)
 
 function loadStories() {
   req.keys().forEach(filename => req(filename))
 }
+
+addParameters({
+  options: {
+    name: 'Manchester',
+    url: 'https://github.com/mmmurray/manchester',
+    theme: themes.dark,
+  },
+})
 
 configure(loadStories, module)
