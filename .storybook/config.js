284e57c9b7d216ec74a5beac67d4d7640f648ee4
Mark Murray <mark@murray.xyz>
2019-06-01 13:19:43 +0100

    build(storybook): add default theme decorator to all stories

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,18 @@
-import { addParameters, configure } from '@storybook/react'
+import { addDecorator, addParameters, configure } from '@storybook/react'
 import { themes } from '@storybook/theming'
+import React from 'react'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from '../src/theme'
 
 const req = require.context('../src/components', true, /\/stories.tsx$/)
 
+const ThemeDecorator = storyFn => (
+  <ThemeProvider theme={defaultTheme}>{storyFn()}</ThemeProvider>
+)
+
 function loadStories() {
+  addDecorator(ThemeDecorator)
+
   req.keys().forEach(filename => req(filename))
 }
 
