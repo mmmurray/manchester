c980d034bbf3b2c978bf7139523d687c6a491dae
Mark Murray <mark@murray.xyz>
2019-06-01 16:42:01 +0100

    test(text-input): added test for default TextInput value

--- src/components/text-input/tests/value.test.tsx
+++ src/components/text-input/tests/value.test.tsx
@@ -2,6 +2,14 @@ import React from 'react'
 import TextInput from '..'
 import { render } from '../../../test-utils'
 
+test('default value is empty', () => {
+  const { getByLabelText } = render(<TextInput label="test input" />)
+
+  const input = getByLabelText('test input') as HTMLInputElement
+
+  expect(input.value).toBe('')
+})
+
 test('can set the current value', () => {
   const { getByLabelText } = render(
     <TextInput label="test input" value="foo" />,
