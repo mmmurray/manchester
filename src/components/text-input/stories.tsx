81831eb185e9bf66941800deaf896a2fc42da961
Mark Murray <mark@murray.xyz>
2019-06-01 14:53:15 +0100

    test(text-input): added empty TextInput story

--- src/components/text-input/stories.tsx
+++ src/components/text-input/stories.tsx
@@ -1,8 +1,31 @@
 import { action } from '@storybook/addon-actions'
 import { storiesOf } from '@storybook/react'
-import React from 'react'
+import React, { FC, useState } from 'react'
 import TextInput from '.'
 
-storiesOf('text-input', module).add('default', () => (
-  <TextInput label="Full name" onChange={action('change')} />
+type StatefulTextInputProps = {
+  initialValue?: string
+}
+
+const StatefulTextInput: FC<StatefulTextInputProps> = ({
+  initialValue = '',
+}) => {
+  const [value, setValue] = useState(initialValue)
+  const onChangeAction = action('change')
+
+  return (
+    <TextInput
+      label="Full name"
+      value={value}
+      onChange={value => {
+        onChangeAction(value)
+        setValue(value)
+      }}
+    />
+  )
+}
+
+storiesOf('text-input', module).add('empty', () => <StatefulTextInput />)
+storiesOf('text-input', module).add('with value', () => (
+  <StatefulTextInput initialValue="Alan Turing" />
 ))
