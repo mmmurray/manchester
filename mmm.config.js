875516e2c23b589de6d5922553796a02fbd7e782
Mark Murray <mark@murray.xyz>
2019-07-14 08:38:19 +0100

    test: restore coverage threshold to 100

--- mmm.config.js
+++ mmm.config.js
@@ -2,13 +2,5 @@ module.exports = {
   transformJestConfig: config => ({
     ...config,
     collectCoverageFrom: [...config.collectCoverageFrom, '!**/stories.tsx'],
-    coverageThreshold: {
-      global: {
-        branches: 0,
-        functions: 0,
-        lines: 0,
-        statements: 0,
-      },
-    },
   }),
 }
