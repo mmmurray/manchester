5b247d3425e29a27e0a821aed3ba90504f7eb5a9
Mark Murray <mark@murray.xyz>
2019-09-03 21:14:53 +0100

    fix: remove global cursor reset

--- styleguide/styleguide-wrapper.tsx
+++ styleguide/styleguide-wrapper.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect } from 'react'
 import styled, { ThemeProvider } from 'styled-components'
 import { defaultTheme } from '../src/theme'
 
@@ -12,10 +12,27 @@ const FauxGlobal = styled.div`
   }
 `
 
-const Wrapper: FC = ({ children }) => (
-  <ThemeProvider theme={defaultTheme}>
-    <FauxGlobal>{children}</FauxGlobal>
-  </ThemeProvider>
-)
+const useRemoveGlobalCursorResetHack = () => {
+  useEffect(() => {
+    const styleElement = document.querySelector(
+      '[data-meta="jss-isolate"]',
+    ) as (HTMLStyleElement & { cursorRemoved?: boolean })
+
+    if (!styleElement.cursorRemoved) {
+      styleElement.cursorRemoved = true
+      ;(styleElement.sheet as any).rules[0].style.cursor = 'inherit'
+    }
+  }, [])
+}
+
+const Wrapper: FC = ({ children }) => {
+  useRemoveGlobalCursorResetHack()
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <FauxGlobal>{children}</FauxGlobal>
+    </ThemeProvider>
+  )
+}
 
 export default Wrapper
